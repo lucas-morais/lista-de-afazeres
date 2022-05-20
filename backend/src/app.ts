41d32129ec6a628d5060569289c5b5f2bd1b9774
Lucas Toledo <lucasmoraisss@gmail.com>
2022-05-20 07:40:14 -0300

    feat: express server configuration

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+export default class App {
+  public app = express();
+
+  constructor() {
+    this.init();
+  }
+
+  private init() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+
+  public start(port: string | number) {
+    this.app.listen(Number(port), () => {
+      console.log(`app rodando na porta ${port}`);
+    })
+  }
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import App from './app'
+
+const PORT = process.env.PORT || 3001;
+
+const server = new App();
+
+ 
+server.start(PORT);
